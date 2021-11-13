be7b13c1289aaebfbf0162c6e574a50902c74524
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-11-13 19:24:24 -0300

    mudei a versao do react router dom e criei o component container

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Provider from './context/Provider';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
